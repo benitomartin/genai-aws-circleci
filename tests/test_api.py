97641bab74adda287d9110626be9d1afdc68a340
benitomartin <bmartinc80@gmail.com>
2025-03-01 16:14:55 +0100

    SAM Deployment Added Secrets

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,11 +2,11 @@ from fastapi.testclient import TestClient
 from main import app
 import pytest
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+# OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 @pytest.fixture
 def client():
